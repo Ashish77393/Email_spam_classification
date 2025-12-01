e1ec2c9a9c15d707ec400b098bd125d13f62a4b8
Ashish77393 <ashishkumar77393@gmail.com>
2025-12-01 14:10:23 +0530

    spam classification

--- spamClassification/settings.py
+++ spamClassification/settings.py
@@ -25,11 +25,7 @@ SECRET_KEY = 'django-insecure-ncp#ps*9tz3mxo*n!ae%-7j*288hdlv@ivh3okrv4329k5=qj=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    'email-spam-classification-1-wtdd.onrender.com',
-    '127.0.0.1',
-    'localhost'
-]
+ALLOWED_HOSTS = ['*']
 
 
 
